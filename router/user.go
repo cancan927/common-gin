472878edf16465ddda4402e3f949ee9d279a9c13
zhangcc <felicityzhangcc@foxmail.com>
2023-04-14 20:57:47 +0800

    把用户注册加到路由中去

---
+++ router/user.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	v1 "github.com/cancan927/common-gin/api/v1"
+	"github.com/gin-gonic/gin"
+)
+
+func InitUserRouters() {
+	//注册user模块路由
+	RegisterRoute(func(rgPublic *gin.RouterGroup, rgAuth *gin.RouterGroup) {
+
+		//登陆是公共路由
+		public := rgPublic.Group("")
+		{
+			public.POST("/register", v1.UserRegisterHandler)
+			//public.GET("/login", v1.UserLoginHandler)
+		}
+
+		//需要鉴权的user分组
+		//user := rgAuth.Group("user")
+		//{
+		//	//user.GET("/:id", v1.UserInfoByIdHandler)
+		//}
+
+	})
+}
